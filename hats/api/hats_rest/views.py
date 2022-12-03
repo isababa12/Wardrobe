f5986faaf625aeb4105cec886b56e51467fd3706
Ryan <ryanlu294@gmail.com>
2022-12-02 19:00:49 -0800

    adds delete and put requests to hat views

--- hats/api/hats_rest/views.py
+++ hats/api/hats_rest/views.py
@@ -47,7 +47,7 @@ def api_list_hats(request, location_vo_id=None):
             content["location"] = location
         except LocationVO.DoesNotExist:
             return JsonResponse(
-                {"message": "Invalid location id"},
+                {"message": "Invalid location href"},
                 status=400,
             )
 
@@ -58,11 +58,33 @@ def api_list_hats(request, location_vo_id=None):
             safe=False,
         )
 
-
+@require_http_methods(["DELETE", "GET", "PUT"])
 def api_show_hat(request, pk):
-    hat = Hat.objects.get(id=pk)
-    return JsonResponse(
-        hat,
-        encoder=HatDetailEncoder,
-        safe=False,
-    )
+    if request.method == "GET":
+        hat = Hat.objects.get(id=pk)
+        return JsonResponse(
+            hat,
+            encoder=HatDetailEncoder,
+            safe=False,
+        )
+    elif request.method == "DELETE":
+        count, _ = Hat.objects.filter(id=pk).delete()
+        return JsonResponse({"deleted": count > 0})
+    else:
+        content = json.loads(request.body)
+        try:
+            if "location" in content:
+                location = LocationVO.objects.get(import_href=content["location"])
+                content["location"] = location
+        except LocationVO.DoesNotExist:
+            return JsonResponse(
+                {"message": "Invalid location href"},
+                status=400,
+            )
+        Hat.objects.filter(id=pk).update(**content)
+        hat = Hat.objects.get(id=pk)
+        return JsonResponse(
+            hat,
+            encoder=HatDetailEncoder,
+            safe=False,
+        )
